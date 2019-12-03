f6d228d5a758acc885146eb2fae3fd41624ec54e
Joey Register <josephregister1258@gmail.com>
2019-12-03 16:44:23 -0500

    hid startquiz button, adding form

--- quiz.js
+++ quiz.js
@@ -1,3 +1,8 @@
+var startGame = document.getElementById("start")
+var totalSeconds = 0
+var timeElapsed = 0
+var quizArea = document.getElementById("quizArea")
+var listItem = document.createElement("li")
 var questions = [
     { title: "Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
@@ -10,3 +15,14 @@ var questions = [
   }
 ]
 
+startGame.addEventListener("click", function(){
+
+  startGame.classList.add("d-none")
+  quizArea.classList.remove("d-none")
+
+  quizArea.textContent = questions[0].title
+  quizArea.appendChild(listItem)
+  quizArea.querySelector("li").textContent = questions[0].choices;
+
+}
+)
