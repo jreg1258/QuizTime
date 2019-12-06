b974477712b71e456c8066017ae9b4caad4f38e0
Joey Register <josephregister1258@gmail.com>
2019-12-05 21:58:46 -0500

    working functions and timer

--- quiz.js
+++ quiz.js
@@ -2,6 +2,7 @@ var startGame = document.getElementById("start")
 var next = document.getElementById("next")
 var totalSeconds = 0
 var timeElapsed = 0
+var clock = null
 var timer = document.getElementById("timer")
 var completeTxt = document.getElementById("complete")
 var quizArea = document.getElementById("quizArea")
@@ -50,33 +51,24 @@ startGame.addEventListener("click", function(){
   localStorage.setItem("score", 0)
 
   totalSeconds = 180
-  display = timer
-  startTimer(totalSeconds,display)
+  clock = startTimer(totalSeconds)
 })
 
-function startTimer(duration, display) {
-  var timer = duration
-  var chkGuess = guess
-  var chkAns = ans
+function startTimer(duration) {
+  return setInterval(function () {
+   timer.textContent = duration--;
 
-  setInterval(function () {
-     
-   display.textContent = duration--;
-
-   if (--timer < 0) {
-          timer = duration;
-      } 
+   if (duration === 0) {
+      clearInterval(clock)
+    } 
   }, 1000);
 }
 
+//start the quiz, show the quiz, and start timer ^
 
-function checkAns() {
-  if (guess!==ans) {
-    event.preventDefault()
-    invalid.classList.add("d-none")
-  } else {quesNum++
-  checkQues()
-}}
+
+
+
 
 function checkRadios() {
   for (var i = 0; i < radios.length; i++) {
@@ -84,39 +76,72 @@ function checkRadios() {
         guess = labels[i].textContent
         console.log(guess)
         checkAns()
+        console.log("hey")
         radios[i].checked = false
     }else if (!radios[i].checked) {
         invalid.classList.remove("d-none")
       }}}
-  
 
 
   
+function checkAns() {
+        if (guess!==ans) {
+          event.preventDefault()
+          invalid.classList.remove("d-none")
+        }
+          quesNum++
+          checkQues()
+        }
+
+
+
 
 function checkQues() {
   if(quesNum===questions.length){
     quizArea.classList.add("d-none")
     completeTxt.classList.remove("d-none")
    } else {
+    
         quesLabel.textContent = questions[quesNum].title
         label1.textContent = questions[quesNum].choices[0]
         label2.textContent = questions[quesNum].choices[1]
         label3.textContent = questions[quesNum].choices[2]
         label4.textContent = questions[quesNum].choices[3] 
         localStorage.setItem("answer", questions[quesNum].answer)
-        
+        ans = questions[quesNum].answer
+        hideInvalid()
       }
 }   
+
+
+
+function hideInvalid() {
+  invalid.classList.add("d-none")
+}
+
+
+
+
 
 next.addEventListener("click", function() {    
             
   checkRadios()
-  
-  event.preventDefault()
 
+
+if (guess!==ans) {
+    clearInterval(clock)
+    var timer3 = parseInt(timer.textContent.trim())
+    totalSeconds = timer3 - 12
+    clock = startTimer(totalSeconds,timer)
+  }
+  event.preventDefault()
 
           })
 
           
+quizArea.addEventListener("change", function() {
 
+  hideInvalid()
+
+})
                 
